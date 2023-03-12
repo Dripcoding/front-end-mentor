33fa36f2528cfd8395b80ec157167d6e5b62826e
Dripcoding <dripcoding@gmail.com>
2023-03-11 17:13:22 -0800

    fix

---
+++ todo-app/lighthouse-mobile.js
@@ -0,0 +1,25 @@
+module.exports = {
+	ci: {
+		healthcheck: {
+			fatal: true,
+			checks: ['githubToken'],
+		},
+		collect: {
+			staticDistDir: './dist',
+			startServerCommand: 'node server.js',
+			numberOfRuns: 5,
+		},
+		upload: {
+			target: 'filesystem',
+			outputDir: '/tmp/lighthouseci',
+		},
+		assert: {
+			assertions: {
+				'categories:performance': ['error', { minScore: 0.7 }],
+				'categories:accessibility': ['error', { minScore: 0.7 }],
+				'categories:best-practices': ['error', { minScore: 0.7 }],
+				'categories:seo': ['error', { minScore: 0.9 }],
+			},
+		},
+	},
+};
