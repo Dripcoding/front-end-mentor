981912096bce030df4ec00f0ff51c31d8c65b41f
Dripcoding <dripcoding@gmail.com>
2023-03-12 15:48:34 -0700

    todo-app: update lighthouse desktop

--- lighthouse-desktop.js
+++ lighthouse-desktop.js
@@ -8,7 +8,7 @@ module.exports = {
 			numberOfRuns: 5,
 			startServerCommand: 'node server.js',
 			settings: {
-				emulatedFormFactor: 'desktop',
+				formFactor: 'desktop',
 			},
 		},
 		upload: {
