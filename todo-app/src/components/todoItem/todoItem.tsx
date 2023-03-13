c3522ed6a907f075df4265a8f9d606f2b0d0646b
Dripcoding <dripcoding@gmail.com>
2023-03-13 13:38:33 -0700

    todo-app: add todoItem desktop styles

--- todo-app/src/components/todoItem/todoItem.tsx
+++ todo-app/src/components/todoItem/todoItem.tsx
@@ -1,3 +1,5 @@
+import '../../styles/todoItem.scss';
+
 const STYLE_BASE = 'TODO_ITEM_';
 
 interface ITodoItem {
@@ -12,11 +14,13 @@ interface ITodoItemProps {
 
 const TodoItem = ({ todo }: ITodoItemProps): JSX.Element => {
 	return (
-		<div className={`${STYLE_BASE}todo_item`} data-testid='TODO_ITEM_CONTAINER'>
-			<div
-				className={`${STYLE_BASE}radio_btn`}
-				data-testid='TODO_ITEM_BTN'
-			></div>
+		<div className={`${STYLE_BASE}container`} data-testid='TODO_ITEM_CONTAINER'>
+			<div className={`${STYLE_BASE}radio_btn_container`}>
+				<div
+					className={`${STYLE_BASE}radio_btn`}
+					data-testid='TODO_ITEM_BTN'
+				></div>
+			</div>
 			<span data-testid='TODO_ITEM_VALUE'>{todo.value}</span>{' '}
 		</div>
 	);
