b0ad300cbc24a81ff258d1e208ee16ec8fa55aed
Dripcoding <dripcoding@gmail.com>
2023-03-19 15:50:57 -0700

    optimize accessibility reporting

--- todo-app/src/main.tsx
+++ todo-app/src/main.tsx
@@ -1,13 +1,20 @@
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import axe from '@axe-core/react';
 
 import { TodoProvider } from 'context/context';
 import App from './app/app';
 
-if (process.env.NODE_ENV !== 'production') {
-	axe(React, ReactDOM, 1000);
-}
+export const reportAccessibility = async (
+	App: typeof React,
+	config?: Record<string, unknown>
+): Promise<void> => {
+	if (process.env.NODE_ENV !== 'production') {
+		const axe = await import('@axe-core/react');
+		const ReactDOM = await import('react-dom');
+
+		axe.default(App, ReactDOM, 1000, config);
+	}
+};
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -20,3 +27,5 @@ root.render(
 		</TodoProvider>
 	</StrictMode>
 );
+
+reportAccessibility(React);
